8fc9fcf81b454a3c0bef1e99ff9c35f819bf9e62
comus3 <come.plantin.carrenard@gmail.com>
2023-09-29 14:28:48 +0200

    would you look at that im already done (:

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+import string
+import time
+from os import system, name
+
+
+def init(desired,max,d,c):
+    global delay
+    global alphabet
+    global desiredString
+    global lettersMax
+    global clear
+    clear = c
+    delay = d
+    lettersMax = max
+    alphabet = string.ascii_letters
+    desiredString = desired
+
+def letter(lettre,actuPrint):
+    i = 0
+    randChar = chr(32)
+    while i<lettersMax and randChar != lettre:
+        i = i + 1
+        randChar = alphabet[random.randint(0,len(alphabet)-1)]
+        actuPrint = actuPrint + randChar
+        print(actuPrint)
+        if randChar!=lettre:actuPrint = actuPrint[:-1]
+        else: return actuPrint
+    actuPrint = actuPrint + lettre
+    print(actuPrint)
+    return actuPrint
+
+def clearT():
+    # for windows
+   if name == 'nt':
+        _ = system('cls')
+   # for mac and linux
+   else:
+        _ = system('clear')
+    
+
+def hackerPrint(string,max,d,clear):
+    init(string,max,d,clear)
+    actuPrint = ""
+    while actuPrint != desiredString:
+        if clear:clearT()
+        lettre = desiredString[len(actuPrint)]
+        actuPrint = letter(lettre,actuPrint)
+        time.sleep(1)
+        
+
+
+if __name__ == '__main__':
+    hackerPrint('Hello World',10,1,True)
+    
+    
+
+
+
+
+
+
+    
+    
